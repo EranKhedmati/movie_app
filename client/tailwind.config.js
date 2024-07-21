f088aa2d53a2f7ee1879e61e0112144729cf3be3
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-07-22 03:17:27 +0330

    make font size responsive and add color pallets

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,8 +7,21 @@ export default {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        '2xl': "clamp(1.5rem , 1vw + 1rem, 2rem)",
+        'xl': "clamp(1rem , 1vw + 0.5rem, 1.5rem)",
+        'lg': "clamp(0.8rem , 1vw + 0.3rem, 1.2rem)",
       }
     },
+    colors: {
+      "red": {
+        500: "#FF0000"
+      },
+      "gray": {
+        300: "#D9D9D9"
+      }
+    }
   },
   plugins: [],
 }
