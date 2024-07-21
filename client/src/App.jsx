b42e905ad78b596cb3ece5f6b049cea94318c3f8
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-07-22 03:08:10 +0330

    install font awesome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+// Font Awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 function App() {
   return (
-    <div className='text-3xl text-red-500 font-poppins'>App</div>
+    <div className='text-3xl text-red-500 font-poppins'>
+      APP
+      <FontAwesomeIcon icon={faCoffee} />
+    </div>
   )
 }
 
