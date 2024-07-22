1c7a92e62bb3b9e7b3d68e37b2451e8443ec0b57
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-07-22 03:33:43 +0330

    install and setup react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-// Font Awesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+// Components
+import Header from "../components/Header";
+import Home from "../components/Home";
 
 function App() {
   return (
-    <div className='text-3xl text-red-500 font-poppins'>
-      APP
-      <FontAwesomeIcon icon={faCoffee} />
+    <div className="font-poppins">
+      {/* Header */}
+      <Header />
+      {/* Routes */}
+      <Routes>
+        {/* This route is example */}
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
