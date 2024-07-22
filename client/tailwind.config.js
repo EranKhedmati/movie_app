1084798973c64507942bd688470a906db5592e93
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-07-22 04:03:47 +0330

    make colors to extended in tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/App.jsx"
+    "./src/App.jsx",
+    "./src/components/Header.jsx"
   ],
   theme: {
     extend: {
@@ -12,16 +13,16 @@ export default {
         '2xl': "clamp(1.5rem , 1vw + 1rem, 2rem)",
         'xl': "clamp(1rem , 1vw + 0.5rem, 1.5rem)",
         'lg': "clamp(0.8rem , 1vw + 0.3rem, 1.2rem)",
+      },
+      colors: {
+        "red": {
+          500: "#FF0000"
+        },
+        "gray": {
+          300: "#D9D9D9"
+        }
       }
     },
-    colors: {
-      "red": {
-        500: "#FF0000"
-      },
-      "gray": {
-        300: "#D9D9D9"
-      }
-    }
   },
   plugins: [],
 }
