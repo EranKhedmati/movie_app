dca1ea430fb647d1767e8448da7972387ac8d07e
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-07-22 03:39:29 +0330

    move component folder to right path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 // Components
-import Header from "../components/Header";
-import Home from "../components/Home";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
